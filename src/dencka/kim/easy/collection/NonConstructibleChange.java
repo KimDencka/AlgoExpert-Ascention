64efe96db6e387abe3a73dd1851d875fc024811f
Denis Kim <densungkim@gmail.com>
2022-08-24 20:22:41 +0500

    [new] NonConstructibleChange

---
+++ src/dencka/kim/easy/collection/NonConstructibleChange.java
@@ -0,0 +1,20 @@
+package dencka.kim.easy.collection;
+
+import java.util.Arrays;
+
+public class NonConstructibleChange {
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 1, 1, 2, 3, 22};
+        System.out.println(nonConstructibleChange(nums));
+    }
+
+    public static int nonConstructibleChange(int[] coins) {
+        Arrays.sort(coins);
+        int cnt = 0;
+        for (int coin : coins) {
+            if (coin > cnt + 1) break;
+            cnt += coin;
+        }
+        return cnt + 1;
+    }
+}
