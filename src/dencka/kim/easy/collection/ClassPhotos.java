c47f03744e2e4ebdd8be0ee1dd5f26a05c31c66a
Denis Kim <densungkim@gmail.com>
2022-09-06 22:07:07 +0500

    [new] ClassPhotos

---
+++ src/dencka/kim/easy/collection/ClassPhotos.java
@@ -0,0 +1,26 @@
+package dencka.kim.easy.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ClassPhotos {
+    public static void main(String[] args) {
+        ArrayList<Integer> red = new ArrayList<>(Arrays.asList(2, 4, 5, 6, 9));
+        ArrayList<Integer> blue = new ArrayList<>(Arrays.asList(1, 3, 4, 5, 8));
+
+        System.out.println(classPhotos(red, blue));
+    }
+
+    public static boolean classPhotos(ArrayList<Integer> redShirtHeights, ArrayList<Integer> blueShirtHeights) {
+        Collections.sort(redShirtHeights);
+        Collections.sort(blueShirtHeights);
+        int cnt = 0;
+        for (int i = 0; i < redShirtHeights.size(); i++) {
+            if (redShirtHeights.get(i) > blueShirtHeights.get(i)) cnt++;
+            else if (redShirtHeights.get(i) < blueShirtHeights.get(i)) cnt--;
+            else return false;
+        }
+        return Math.abs(cnt) == redShirtHeights.size();
+    }
+}
