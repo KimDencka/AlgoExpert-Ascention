9d613d49ee53b48e992853eba648b38c34834c53
Denis Kim <densungkim@gmail.com>
2022-09-07 15:45:39 +0500

    [new] BinarySearch

---
+++ src/dencka/kim/easy/collection/BinarySearch.java
@@ -0,0 +1,22 @@
+package dencka.kim.easy.collection;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 21, 33, 45, 45, 61, 71, 72, 73};
+
+        System.out.println(binarySearch(nums, 33));
+    }
+
+    public static int binarySearch(int[] array, int target) {
+        int low = 0;
+        int high = array.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (array[mid] == target) return mid;
+
+            if (array[mid] > target) high = mid - 1;
+            else low = mid + 1;
+        }
+        return -1;
+    }
+}
