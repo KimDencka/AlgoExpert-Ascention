3f65b2a3a326f951719be95dc3bcac5f0f7507be
Denis Kim <densungkim@gmail.com>
2022-09-07 17:01:50 +0500

    [new] InsertionSort

---
+++ src/dencka/kim/easy/collection/InsertionSort.java
@@ -0,0 +1,24 @@
+package dencka.kim.easy.collection;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] input = {8, 5, 2, 9, 5, 6, 3};
+        System.out.println(Arrays.toString(insertionSort(input)));
+    }
+
+    public static int[] insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+        return array;
+    }
+}
