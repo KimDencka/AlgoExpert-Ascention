28df2084baaef8f895ccd306905ffe23a0101cee
Denis Kim <densungkim@gmail.com>
2022-09-07 17:52:24 +0500

    [new] CaesarCipherEncryptor

---
+++ src/dencka/kim/easy/other/CaesarCipherEncryptor.java
@@ -0,0 +1,20 @@
+package dencka.kim.easy.other;
+
+public class CaesarCipherEncryptor {
+    public static void main(String[] args) {
+        System.out.println(caesarCypherEncryptor("xyz", 2));
+    }
+
+    public static String caesarCypherEncryptor(String str, int key) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            int code = str.charAt(i) + (key % 26);
+            if (code > 'z') {
+                code -= 26;
+            }
+
+            res.append((char) code);
+        }
+        return res.toString();
+    }
+}
