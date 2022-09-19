d20eeb3a2ce3cdcb758c042255ef6b2b315da110
Denis Kim <densungkim@gmail.com>
2022-09-19 22:20:52 +0500

    [new] ValidateBST

---
+++ src/dencka/kim/medium/tree/ValidateBST.java
@@ -0,0 +1,24 @@
+package dencka.kim.medium.tree;
+
+class ValidateBST {
+    public static boolean validateBst(BST tree) {
+        return helper(tree, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    public static boolean helper(BST tree, int min, int max) {
+        if (tree == null) return true;
+        if (tree.value < min || tree.value >= max) return false;
+        boolean leftIsValid = helper(tree.left, min, tree.value);
+        return leftIsValid && helper(tree.right, tree.value, max);
+    }
+
+    static class BST {
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value) {
+            this.value = value;
+        }
+    }
+}
