04f2a50eb39c831cf9ac47aa058cd83252767ad1
Denis Kim <densungkim@gmail.com>
2022-09-19 20:21:08 +0500

    [new] MergeOverlappingIntervals

---
+++ src/dencka/kim/medium/collection/MergeOverlappingIntervals.java
@@ -0,0 +1,23 @@
+package dencka.kim.medium.collection;
+
+import java.util.*;
+
+class MergeOverlappingIntervals {
+    public static void main(String[] args) {
+        int[][] input = {{1, 2}, {3, 5}, {4, 7}, {6, 8}, {9, 10}};
+        System.out.println(Arrays.deepToString(mergeOverlappingIntervals(input)));
+    }
+
+    public static int[][] mergeOverlappingIntervals(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparing(a -> a[0]));
+        LinkedList<int[]> merged = new LinkedList<>();
+        for (int[] interval : intervals) {
+            if (merged.isEmpty() || merged.getLast()[1] < interval[0]) {
+                merged.add(interval);
+            } else {
+                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
