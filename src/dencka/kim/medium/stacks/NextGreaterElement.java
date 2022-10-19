07f7717f0ec573665954c707c18d055a4ebce868
Denis Kim <densungkim@gmail.com>
2022-10-19 09:19:30 +0500

    [new] NextGreaterElement

---
+++ src/dencka/kim/medium/stacks/NextGreaterElement.java
@@ -0,0 +1,27 @@
+package dencka.kim.medium.stacks;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Stack;
+
+class NextGreaterElement {
+    public static void main(String[] args) {
+        int[] input = new int[]{2, 5, -3, -4, 6, 7, 2};
+        System.out.println(Arrays.toString(nextGreaterElement(input)));
+    }
+
+    public static int[] nextGreaterElement(int[] array) {
+        int[] result = new int[array.length];
+        Arrays.fill(result, -1);
+        Stack<Integer> indices = new Stack<>();
+        for (int i = 0; i < 2 * array.length; i++) {
+            int idx = i % array.length;
+            while (!indices.isEmpty() && array[indices.peek()] < array[idx]) {
+                int front = indices.pop();
+                result[front] = array[idx];
+            }
+            indices.push(idx);
+        }
+        return result;
+    }
+}
